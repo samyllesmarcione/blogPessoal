29607e8f572e78bb854b9516102a68a47f2d97ff
Samylles Marcione <samyllesmarcione00@gmail.com>
2022-06-09 13:51:26 -0300

    Atualizações

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
 import { Usuario } from '../model/Usuario';
 import { UsuarioLogin } from '../model/UsuarioLogin';
 
@@ -21,7 +22,14 @@ export class AuthService {
 
   cadastrar( usuario: Usuario): Observable<Usuario>{
     return this.http.post<Usuario>('https://blogsamsam.herokuapp.com/usuarios/cadastrar', usuario)
+  }
 
-    
+  logado(){
+    let ok: boolean = false
+
+    if(environment.token != ''){
+      ok = true
+    }
+    return ok 
   }
 }
